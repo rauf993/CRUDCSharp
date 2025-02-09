7f9b5936fda85b06e943bffed98b1ace653d83d5
rauf993 <raufalfonso@gmail.com>
2025-02-08 18:51:09 -0600

    Removed .vs folder from repository

---
+++ CRUD/database/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+
+namespace CRUD.database
+{
+    internal class Class1
+    {
+        protected SqlConnection connect;
+        string connectionString = "Data Source=DESKTOP-VCTIH2D\\SQLEXPRESS;Initial Catalog=rauf;Integrated Security=True;TrustServerCertificate=True;";
+
+        protected async Task OpenConnectionAsync()
+        {
+            try
+            {
+                connect = new SqlConnection(connectionString);
+                await connect.OpenAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("theres an errxxxor" + e.Message);
+            }
+        }
+        protected void CloseConnection()
+        {
+            if (connect != null)
+            {
+                connect.Close();  
+                connect.Dispose(); 
+            }
+        }
+    }
+
+
+
+}
+
